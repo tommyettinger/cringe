78b677beb75869af7cc16c5767d30118b055331c
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-01-13 22:17:16 -0800

    Add barronSpline() and more number parsing.

--- src/main/java/com/github/tommyettinger/cringe/MathSupport.java
+++ src/main/java/com/github/tommyettinger/cringe/MathSupport.java
@@ -161,4 +161,174 @@ public final class MathSupport {
         return x;
     }
 
+
+    /**
+     * A generalization on bias and gain functions that can represent both; this version is branch-less.
+     * This is based on <a href="https://arxiv.org/abs/2010.09714">this micro-paper</a> by Jon Barron, which
+     * generalizes the earlier bias and gain rational functions by Schlick. The second and final page of the
+     * paper has useful graphs of what the s (shape) and t (turning point) parameters do; shape should be 0
+     * or greater, while turning must be between 0 and 1, inclusive. This effectively combines two different
+     * curving functions so that they continue into each other when x equals turning. The shape parameter will
+     * cause this to imitate "smoothstep-like" splines when greater than 1 (where the values ease into their
+     * starting and ending levels), or to be the inverse when less than 1 (where values start like square
+     * root does, taking off very quickly, but also end like square does, landing abruptly at the ending
+     * level). You should only give x values between 0 and 1, inclusive.
+     *
+     * @param x       progress through the spline, from 0 to 1, inclusive
+     * @param shape   must be greater than or equal to 0; values greater than 1 are "normal interpolations"
+     * @param turning a value between 0.0 and 1.0, inclusive, where the shape changes
+     * @return a float between 0 and 1, inclusive
+     */
+    public static float barronSpline(final float x, final float shape, final float turning) {
+        final float d = turning - x;
+        final int f = NumberUtils.floatToIntBits(d) >> 31, n = f | 1;
+        return (turning * n - f) * (x + f) / (1.17549435E-38f - f + (x + shape * d) * n) - f;
+    }
+    // 1.17549435E-38f is 0x1p-126f
+
+    /**
+     * An overly-permissive, but fast, way of looking up the numeric value of a hex digit provided as a char.
+     * This does not use a table lookup. It will return garbage if not given a valid hex digit, but will not crash
+     * or throw an Exception on any input. If you know the given digit is between 0 and 9 inclusive, this can also be
+     * used to get the numeric value of that digit as decimal, rather than hexadecimal. You could instead use
+     * {@code (c & 15)} or just {@code (c - '0')} in that case, though.
+     * @param c a char that should really be a valid hex digit (matching the regex {@code [0-9A-Fa-f]})
+     * @return the numeric value of the given digit char
+     */
+    public static int hexCode(final char c) {
+        // this will be 0 when c is between 0-9, and 64 when c is any letter.
+        final int h = (c & 64);
+        // the bottom bits (going up to 15) are accurate for 0-9, but are 9 off for letters.
+        // (64 >>> 3) is 8, and (64 >>> 6) is 1.
+        return (c & 15) + (h >>> 3) + (h >>> 6);
+    }
+
+    /**
+     * Reads in a CharSequence containing only decimal digits (only 0-9) with an optional sign at the start
+     * and returns the int they represent, reading at most 10 characters (11 if there is a sign) and returning the
+     * result if valid, or 0 if nothing could be read. The leading sign can be '+' or '-' if present. This can also
+     * represent negative numbers as they are printed as unsigned integers. This means "4294967295" would return the int
+     * -1 when passed to this, though you could also simply use "-1" . If you use both '-' at the start and have the
+     * number as greater than {@link Integer#MAX_VALUE}, such as with "-4294967295", then both indicate a negative
+     * number, but the digits will be processed first (producing -1) and then the whole thing will be multiplied by -1
+     * to flip the sign again (returning 1).
+     * <br>
+     * Should be fairly close to Java 8's Integer.parseUnsignedInt method, which is an odd omission from earlier JDKs.
+     * This doesn't throw on invalid input, though, instead returning 0 if the first char is not a decimal digit, or
+     * stopping the parse process early if a non-0-9 char is read before end is reached. If the parse is stopped
+     * early, this behaves as you would expect for a number with fewer digits, and simply doesn't fill the larger places.
+     *
+     * @param cs    a CharSequence, such as a String, containing decimal digits with an optional sign
+     * @param start the (inclusive) first character position in cs to read
+     * @param end   the (exclusive) last character position in cs to read (this stops after 10 characters if end is too large)
+     * @return the int that cs represents
+     */
+    public static int intFromDec(final CharSequence cs, final int start, int end) {
+        int sign, h, lim, len;
+        if (cs == null || start < 0 || end <= 0 || end - start <= 0
+                || (len = cs.length()) - start <= 0 || end > len)
+            return 0;
+        char c = cs.charAt(start);
+        if (c == '-') {
+            sign = -1;
+            h = 0;
+            lim = 11;
+        } else if (c == '+') {
+            sign = 1;
+            h = 0;
+            lim = 11;
+        } else {
+            if (!(c >= '0' && c <= '9'))
+                return 0;
+            else {
+                sign = 1;
+                lim = 10;
+            }
+            h = (c - '0');
+        }
+        int data = h;
+        for (int i = start + 1; i < end && i < start + lim; i++) {
+            c = cs.charAt(i);
+            if (!(c >= '0' && c <= '9'))
+                return data * sign;
+            data *= 10;
+            data |= (c - '0');
+        }
+        return data * sign;
+    }
+
+    /**
+     * Reads in a CharSequence containing only hex digits (only 0-9, a-f, and A-F) with an optional sign at the start
+     * and returns the int they represent, reading at most 8 characters (9 if there is a sign) and returning the
+     * result if valid, or 0 if nothing could be read. The leading sign can be '+' or '-' if present. This can also
+     * represent negative numbers as they are printed by such methods as String.format() given %X in the formatting
+     * string; that is, if the first char of a 8-char (or longer)
+     * CharSequence is a hex digit 8 or higher, then the whole number represents a negative number, using two's
+     * complement and so on. This means "FFFFFFFF" would return the int -1 when passed to this, though you
+     * could also simply use "-1" . If you use both '-' at the start and have the most significant digit as 8 or higher,
+     * such as with "-FFFFFFFF", then both indicate a negative number, but the digits will be processed first
+     * (producing -1) and then the whole thing will be multiplied by -1 to flip the sign again (returning 1).
+     * <br>
+     * Should be fairly close to Java 8's Integer.parseUnsignedInt method, which is an odd omission from earlier JDKs.
+     * This doesn't throw on invalid input, though, instead returning 0 if the first char is not a hex digit, or
+     * stopping the parse process early if a non-hex-digit char is read before end is reached. If the parse is stopped
+     * early, this behaves as you would expect for a number with fewer digits, and simply doesn't fill the larger places.
+     *
+     * @param cs    a CharSequence, such as a String, containing hex digits with an optional sign (no 0x at the start)
+     * @param start the (inclusive) first character position in cs to read
+     * @param end   the (exclusive) last character position in cs to read (this stops after 8 characters if end is too large)
+     * @return the int that cs represents
+     */
+    public static int intFromHex(final CharSequence cs, final int start, int end) {
+        int sign, h, lim, len;
+        if (cs == null || start < 0 || end <= 0 || end - start <= 0
+                || (len = cs.length()) - start <= 0 || end > len)
+            return 0;
+        char c = cs.charAt(start);
+        if (c == '-') {
+            sign = -1;
+            h = 0;
+            lim = 9;
+        } else if (c == '+') {
+            sign = 1;
+            h = 0;
+            lim = 9;
+        } else {
+            if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')))
+                return 0;
+            else {
+                sign = 1;
+                lim = 8;
+            }
+            h = hexCode(c);
+        }
+        int data = h;
+        for (int i = start + 1; i < end && i < start + lim; i++) {
+            c = cs.charAt(i);
+            if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')))
+                return data * sign;
+            data <<= 4;
+            data |= hexCode(c);
+        }
+        return data * sign;
+    }
+
+    /**
+     * Reads a float in from the String {@code str}, using only the range from {@code start} (inclusive) to {@code end}
+     * (exclusive). This effectively returns {@code Float.parseFloat(str.substring(start, end))} .
+     * Unlike the other number-reading methods here, this doesn't do much to validate its input, so the end must be
+     * after the full float number. If the parse fails, this returns 0f.
+     * @param str a String containing a valid float in the specified range
+     * @param start the start index (inclusive) to read from
+     * @param end the end index (exclusive) to stop reading before
+     * @return the parsed float from the given range, or 0f if the parse failed.
+     */
+    public static float floatFromDec(final String str, final int start, int end) {
+        try {
+            return Float.parseFloat(str.substring(start, end));
+        } catch (NumberFormatException ignored) {
+            return 0f;
+        }
+    }
+
 }

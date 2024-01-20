52fd5062f2a7e505f284cdff77cdcac262228345
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-01-19 22:03:28 -0800

    Add splineWobble() and trigWobble().

--- src/main/java/com/github/tommyettinger/cringe/LineWobble.java
+++ src/main/java/com/github/tommyettinger/cringe/LineWobble.java
@@ -17,6 +17,8 @@
 
 package com.github.tommyettinger.cringe;
 
+import com.badlogic.gdx.math.MathUtils;
+
 /**
  * Provides 1D noise methods that can be queried at any point on a line to get a continuous random value.
  * These each use some form of low-quality, high-speed unary hash on the floor and ceiling of a float or double value,
@@ -86,5 +88,88 @@ public class LineWobble {
         // 7.7.228014483236334E-20 , or 0x1.5555555555428p-64 , is just inside {@code -2f/3f/Long.MIN_VALUE} .
         // it gets us about as close as we can go to 1.0 .
         return (t * (t * t * p + t * (a - b - p) + c - a) + b) * 7.228014E-20f;
+    }
+
+    /**
+     * A variant on {@link #wobble(int, float)} that uses {@link MathSupport#barronSpline(float, float, float)} to
+     * interpolate between peaks/valleys, with the shape and turning point determined like the other values.
+     * This can be useful when you want a curve to seem more "natural," without the similarity between every peak or
+     * every valley in {@link #wobble(int, float)}. This can produce both fairly sharp turns and very gradual curves.
+     * @param seed an int seed that will determine the pattern of peaks and valleys this will generate as value changes; this should not change between calls
+     * @param value a float that typically changes slowly, by less than 2.0, with direction changes at integer inputs
+     * @return a pseudo-random float between -1f and 1f (both exclusive), smoothly changing with value
+     */
+    public static float splineWobble(int seed, float value)
+    {
+        // int fast floor, from libGDX; 16384 is 2 to the 14, or 0x1p14, or 0x4000
+        final int floor = ((int)(value + 16384.0) - 16384);
+        // the three lines below break up multiplications into component parts to avoid precision loss on GWT.
+        final int z = seed + (floor * 0x22179 | 0) * 0x4A41; // this is the same as: seed + floor * 0x9E3779B9
+        final int startBits = ((z ^ 0xD1B54A35) * 0x923EF | 0) * 0x100D | 0; // the same as: (z ^ 0xD1B54A35) * 0x92B5C323
+        final int endBits   = ((z + 0x9E3779B9 ^ 0xD1B54A35) * 0x923EF | 0) * 0x100D | 0; // also * 0x92B5C323
+        final int mixBits = startBits + endBits;
+        value -= floor;
+        value = MathSupport.barronSpline(value,
+                (mixBits & 0xFFFF) * 6.1035156E-5f + 1f, // 6.1035156E-5f == 0x1p-14f
+                (mixBits >>> 16) * 1.1444092E-5f + 0.125f); // 1.1444092E-5f == 0x1.8p-17f
+        value *= value * (3f - 2f * value);
+        return ((1 - value) * startBits + value * endBits) * 4.6566126E-10f; // 4.6566126E-10f == 0x0.ffffffp-31f
+    }
+
+    /**
+     * A variant on {@link #wobble(int, float)} that uses {@link MathSupport#barronSpline(float, float, float)} to
+     * interpolate between peaks/valleys, with the shape and turning point determined like the other values.
+     * This can be useful when you want a curve to seem more "natural," without the similarity between every peak or
+     * every valley in {@link #wobble(int, float)}. This can produce both fairly sharp turns and very gradual curves.
+     * @param seed a long seed that will determine the pattern of peaks and valleys this will generate as value changes; this should not change between calls
+     * @param value a float that typically changes slowly, by less than 2.0, with direction changes at integer inputs
+     * @return a pseudo-random float between -1f and 1f (both exclusive), smoothly changing with value
+     */
+    public static float splineWobble(long seed, float value)
+    {
+        final long floor = ((long)(value + 0x1p14) - 0x4000);
+        final long z = seed + floor * 0x6C8E9CF570932BD5L;
+        final long startBits = ((z ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5C323L ^ 0x9E3779B97F4A7C15L),
+                endBits = ((z + 0x6C8E9CF570932BD5L ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5C323L ^ 0x9E3779B97F4A7C15L),
+                mixBits = startBits + endBits;
+        value -= floor;
+        value = MathSupport.barronSpline(value, (mixBits & 0xFFFFFFFFL) * 0x1p-30f + 1f, (mixBits >>> 32 & 0xFFFFL) * 0x1.8p-17f + 0.125f);
+        value *= value * (3f - 2f * value);
+        return ((1 - value) * startBits + value * endBits) * 0x0.ffffffp-63f;
+    }
+
+    /**
+     * Trigonometric wobble. Domain for {@code value} is effectively [-16384, 16384]. Range is (-1, 1).
+     * @param seed a long seed that will determine the pattern of peaks and valleys this will generate as value changes; this should not change between calls
+     * @param value a float that typically changes slowly, by less than 2.0, with direction changes at integer inputs
+     * @return a pseudo-random float between -1f and 1f (both exclusive), smoothly changing with value
+     */
+    public static float trigWobble(long seed, float value)
+    {
+        final long floor = ((int)(value + 16384.0) - 16384);
+        final long z = seed + floor * 0x6C8E9CF570932BD5L;
+        final long start = ((z ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5C323L),
+                end = ((z + 0x6C8E9CF570932BD5L ^ 0x9E3779B97F4A7C15L) * 0xC6BC279692B5C323L);
+        value = MathUtils.sinDeg((value - floor) * 90);
+        value *= value;
+        return ((1f - value) * start + value * end) * 0x0.ffffffp-63f;
+    }
+
+    /**
+     * Trigonometric wobble. Domain for {@code value} is effectively [-16384, 16384]. Range is (-1, 1).
+     * @param seed an int seed that will determine the pattern of peaks and valleys this will generate as value changes; this should not change between calls
+     * @param value a float that typically changes slowly, by less than 2.0, with direction changes at integer inputs
+     * @return a pseudo-random float between -1f and 1f (both exclusive), smoothly changing with value
+     */
+    public static float trigWobble(int seed, float value)
+    {
+        final int floor = ((int)(value + 0x1p14) - 0x4000);
+        // the three lines below break up multiplications into component parts to avoid precision loss on GWT.
+        final int z = seed + (floor * 0x22179 | 0) * 0x4A41; // this is the same as: seed + floor * 0x9E3779B9
+        final int start = ((z ^ 0xD1B54A35) * 0x923EF | 0) * 0x100D | 0; // the same as: (z ^ 0xD1B54A35) * 0x92B5C323
+        final int end   = ((z + 0x9E3779B9 ^ 0xD1B54A35) * 0x923EF | 0) * 0x100D | 0; // also * 0x92B5C323
+        value = MathUtils.sinDeg((value - floor) * 90);
+        value *= value;
+        return ((1f - value) * start + value * end) * 0x0.ffffffp-31f;
     }
 }
